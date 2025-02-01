af2c08cdeb13597011ac3999b5b8faeb9e7cdb1f
Joshua Pickering <pickeringjoshua1999@gmail.com>
2025-02-01 03:25:44 +0000

    visual updates

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -27,10 +27,25 @@
 		top: 10px;
 		left: 10px;
 		text-decoration: none;
-		color: black;
+		color: #E2BE73;
 		padding: 5px 10px;
 		border-radius: 10px;
-		background-color: #e0e0e0;
+		background-color: white;
 		z-index: 2;
+		border: 3px solid #e0e0e0;
+			box-shadow:
+				4px 4px 8px #cccccc,
+				-4px -4px 8px #ffffff;
+		@media (prefers-color-scheme: dark){
+			background-color: #1D2026;
+			border: 3px solid #212121;
+			box-shadow:
+				4px 4px 8px #0a0a0a,
+				-4px -4px 8px #1a1a1a;
+		}
+	}
+
+	.back:hover {
+		border: 3px solid #E2BE73;
 	}
 </style>
